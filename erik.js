983b62941f7f081207e901b8e5390479752dcca4
levnikolausson <administrator@Administrators-MacBook-Air-3.local>
2014-10-07 12:26:06 +0200

    js, index och sch

--- erik.js
+++ erik.js
@@ -10,3 +10,22 @@
                         $menuLeft.toggleClass('pushmenu-open');
                 });
         });
+
+function fixPicSize()
+{
+var p1 = document.getElementById('left_pic');
+var picHeight = p1.height + "px";
+var bildHeight = p1.height + 6 + "px";
+p1.style.height = picHeight;
+p1 = document.getElementById('middle_pic');
+p1.style.height = picHeight;
+p1 = document.getElementById('right_pic');
+p1.style.height = picHeight;
+p1 = document.getElementById('bild1');
+p1.style.height = bildHeight;
+p1 = document.getElementById('bild2');
+p1.style.height = bildHeight;
+p1 = document.getElementById('bild3');
+p1.style.height = bildHeight;
+
+}
